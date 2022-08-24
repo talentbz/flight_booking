9fbeb562a407a2a187b48f71df7a2a2d17f687ae
rockstar <leragnezdina@mail.ru>
2022-08-24 16:05:52 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Admin profile change
+	change password
+## User management
+	- user list
+	- user add, delete, status (active, inactive)
+## Seat management
+	- seat type (CRUD)
+	- increase price(%, fixed, time, seat count, status)
+## Booking Transaction
+	- list according to every user
+
+
+The following were agreed: 
+
+1. A backend system for travel agents to sell tickets 
+2. Within the shortest possible time, a front-end system for passengers to purchase tickets directly. 
+3. Seats will be sold in tiers, with the system automated to increase prices as available seat numbers reduce and as the departure date draws near. 
+4. The system will allow for a manual override of the automation mentioned in point 3. 
+5. Paystack and Skrill will be integrated as payment gateways 
+
+
---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,62 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="vertical-menu">
+
+    <div data-simplebar class="h-100">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span key="t-dashboards">@lang('translation.Dashboards')</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-store"></i>
+                        <span key="t-ecommerce">Admin</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="javascript: void(0);" key="t-products">Change Profile</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-store"></i>
+                        <span key="t-ecommerce">User Management</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="javascript: void(0);" key="t-products">User List</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-store"></i>
+                        <span key="t-ecommerce">Seat Management</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="javascript: void(0);" key="t-products">Seat Type</a></li>
+                        <li><a href="javascript: void(0);" key="t-products">Price Management</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-store"></i>
+                        <span key="t-ecommerce">Booking History</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="javascript: void(0);" key="t-products">Booking List</a></li>
+                        <li><a href="javascript: void(0);" key="t-products">Price Management</a></li>
+                    </ul>
+                </li>
+
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
+<!-- Left Sidebar End -->
