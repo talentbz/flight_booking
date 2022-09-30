4882251a86a024a3a125e97c35b622a8b9adf0c7
rockstar <leragnezdina@mail.ru>
2022-09-30 17:33:33 +0900

    create home page

--- resources/js/components/Footer/Footer.js
+++ resources/js/components/Footer/Footer.js
@@ -3,6 +3,11 @@ import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import Image from 'react-bootstrap/Image';
 import image_1 from '../../../images/frontend/rect_1.png';
+import image_2 from '../../../images/frontend/rect_2.png';
+import image_3 from '../../../images/frontend/rect_3.png';
+import image_4 from '../../../images/frontend/rect_4.png';
+import image_5 from '../../../images/frontend/rect_5.png';
+import image_6 from '../../../images/frontend/rect_6.png';
 import './Footer.scss';
 
 function Footer(){
@@ -12,21 +17,38 @@ function Footer(){
             title: 'Bed',
         },
         {
-            img: image_1,
+            img: image_2,
             title: 'Bed',
         },
         {
-            img: image_1,
+            img: image_3,
             title: 'Bed',
         },
         {
-            img: image_1,
+            img: image_4,
             title: 'Bed',
         },
         {
-            img: image_1,
+            img: image_5,
             title: 'Bed',
         },
+        {
+            img: image_6,
+            title: 'Bed',
+        },
+        {
+            img: image_6,
+            title: 'Bed',
+        },
+        {
+            img: image_6,
+            title: 'Bed',
+        },
+        {
+            img: image_6,
+            title: 'Bed',
+        },
+
     ];
     return (
         <div className='footer'>
@@ -42,7 +64,7 @@ function Footer(){
                                 <li><Link href="#" >booking</Link></li>
                             </ul>
                         </Grid>
-                        <Grid className='footer-list' item xs={4}>
+                        <Grid className='footer-list follow-us' item xs={4}>
                             <p>FOLLOW US</p>
                             <ul>
                                 <li><Link href="#" >Newsletter</Link></li>
@@ -54,9 +76,9 @@ function Footer(){
                                 <li><Link href="#" >Youtube</Link></li>
                             </ul>
                         </Grid>
-                        <Grid className='footer-list' item xs={4}>
+                        <Grid className='footer-list instagram' item xs={4}>
                             <p>INSTRAGRAM</p>
-                            <Grid container spacing={1}>
+                            <Grid className='instagram-list' container spacing={1}>
                                 {imageData.map((item, index) => (
                                 <Grid item xs={4} key={index}>
                                     <img className="" src={item.img} /> 
@@ -68,7 +90,7 @@ function Footer(){
                 </div>
             </div>
             <div className='footer-copyright'>
-
+                 <p>Copyright © 2022 Numero un consultancy. All rights reserved.</p>                       
             </div>
         </div>
     );
